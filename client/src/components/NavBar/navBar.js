fbd7d5d29e6bb146d12f712ec13e833b31f2c16a
Indresh P <63254562+indreshp135@users.noreply.github.com>
2021-04-07 21:51:03 +0530

    feat(qna): Next button and option selection (#8)
    * feat(qna): Next button and option selection
    
    * remove(wastepage)

--- client/src/components/NavBar/navBar.js
+++ client/src/components/NavBar/navBar.js
@@ -1,43 +1,43 @@
 import React from "react";
 import clsx from 'clsx';
 import {
-	Drawer,
+	// Drawer,
 	AppBar,
 	Toolbar,
 	CssBaseline,
-	List,
+	// List,
 	IconButton,
-  BottomNavigation,
-  BottomNavigationAction
+  // BottomNavigation,
+  // BottomNavigationAction
 } from '@material-ui/core';
 import {isMobile} from "react-device-detect";
 import {useStyles} from "./styles.js";
-import { useTheme } from '@material-ui/core/styles';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+// import { useTheme } from '@material-ui/core/styles';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import AavegLogo from "../../assets/images/aaveg.png"
-import HomeLogo2 from "../../assets/images/homewhite.png"
-import EventsLogo2 from "../../assets/images/calendarwhite.png"
-import ScoreLogo2 from "../../assets/images/leaderboardwhite.png"
-import SponsorLogo2 from "../../assets/images/sponsorswhite.png"
+// import HomeLogo2 from "../../assets/images/homewhite.png"
+// import EventsLogo2 from "../../assets/images/calendarwhite.png"
+// import ScoreLogo2 from "../../assets/images/leaderboardwhite.png"
+// import SponsorLogo2 from "../../assets/images/sponsorswhite.png"
 import "./navBar.css";
 
 export const NavBar = (props) =>{
   const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState('recents');
+  // const theme = useTheme();
+  // const [value, setValue] = React.useState('recents');
 
 
-   const Item = ({icon,name}) => {
-  return ( <div className={classes.navBar}>
-      <div className="imgDiv"><img src={icon} style={{width:'20px',height:'20px'}} alt="aaveg" id="aaveg"/></div>
-      <s2 style={{color:'white'}}>{name}</s2>
-    </div>);
-  };
+  //  const Item = ({icon,name}) => {
+  // return ( <div className={classes.navBar}>
+  //     <div className="imgDiv"><img src={icon} style={{width:'20px',height:'20px'}} alt="aaveg" id="aaveg"/></div>
+  //     <s2 style={{color:'white'}}>{name}</s2>
+  //   </div>);
+  // };
   
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
 
     if(!isMobile)
    {  return (
@@ -58,7 +58,7 @@ export const NavBar = (props) =>{
           </IconButton> 
         </Toolbar>
       </AppBar>
-      <Drawer
+      {/* <Drawer
         variant="permanent"
         className={clsx(classes.drawerClose)}
         classes={{
@@ -77,7 +77,7 @@ export const NavBar = (props) =>{
         <Item icon={ScoreLogo2} name='Scores'/>
         <Item icon={SponsorLogo2} name='Sponsor'/>
         </List>
-      </Drawer>
+      </Drawer> */}
       <main className={classes.content}>
         <div className={classes.contentChild}>{props.children}</div>
       </main>
@@ -105,12 +105,12 @@ export const NavBar = (props) =>{
       <main className={classes.content}>
         <div className={classes.contentChild}>{props.children}</div>
       </main>
-  <BottomNavigation value={value} onChange={handleChange} className={classes.bottomBar}>
+  {/* <BottomNavigation value={value} onChange={handleChange} className={classes.bottomBar}>
      <BottomNavigationAction label="Home" value="home" icon={<img src={HomeLogo2} alt="Home Logo" style={{width:'20px',height:'20px'}}/>}/>
       <BottomNavigationAction label="Events" value="events" icon={<img src={EventsLogo2} alt="Event Logo" style={{width:'20px',height:'20px'}}/>} />
       <BottomNavigationAction label="ScoreBoard" value="score" icon={<img src={ScoreLogo2} alt="Score Logo" style={{width:'20px',height:'20px'}}/>} />
       <BottomNavigationAction label="Sponsors" value="sponsor" icon={<img src={SponsorLogo2} alt="Sponsor Logo" style={{width:'20px',height:'20px'}}/>} />
-    </BottomNavigation>
+    </BottomNavigation> */}
     
     </div>
   );}
--- client/src/pages/QnA/qna.js
+++ client/src/pages/QnA/qna.js
@@ -1,11 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Carousel, { consts } from 'react-elastic-carousel'
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import { FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from '@material-ui/core'
+import { Button, FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from '@material-ui/core'
 import './carouselStyles.css'
 import { useStyles } from "./styles";
 import bg from '../../assets/bg.mp4'
+import { BACKEND_API } from "../../config/config";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
+
+import axios from "axios";
 
 import {qList} from './utils/qList'
 
@@ -19,43 +24,65 @@ const theme = createMuiTheme({
         },
         type: "dark"
     }
-  });
+});
 
 export const QnAPage = () => {
+    const history = useHistory();
     const classes = useStyles();
     const [questions, setQues] = useState([])
+    const [values, setValues] = useState([])
     const [loading, setLoading] = useState(true)
+    const ref = useRef()
 
     const carouselSettings = {
         itemPosition: consts.START,
         focusOnSelect: false,
     };
 
-    const setQuestions = () => {
+    useEffect(() => {
         //TODO: fetch qIds from backend
 
         // temporary lists
-        const qIdList = ["1", "3", "4", "5", "7"]
-        
+        const qIdList = ["1","2", "3", "4", "5", "7"]
+        var val = new Array(qIdList.length).fill(null)
+        setValues(val)
         const ques = []
         qList.forEach(q => {
-        if(qIdList.includes(q.qId)){
-            ques.push(q)
-        }
+            if(qIdList.includes(q.qId)){
+                ques.push(q)
+            }
         })
         setQues(ques)
-        console.log(ques, questions)
         setLoading(false)
-    }
-
-    useEffect(() => {
-        setQuestions()
-        // eslint-disable-next-line
     }, [])
 
     const handleChange = e => {
         const val = e.target.value
-        console.log(val)
+        var value = values
+        var quesId = questions.findIndex((q)=>q.qId===String(parseInt(val.substr(0,2))))
+        value[quesId] = questions[quesId].answers[parseInt(val.substr(2,1))-1].ansId
+        setValues([...value])
+    }
+
+    const submitHandler = () => {
+        console.log(values)
+        
+        axios
+        .post(BACKEND_API + "/api/quiz/saveAnswers", JSON.stringify({questions:values}))
+        .then((res) => {
+            if (res.status_code === 200) {
+            toast.success("logged in/ get from res", {
+                position: "bottom-center",
+            });
+            history.push("/submitted");
+            } else {
+            toast.error("error/get from res", { position: "bottom-center" });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            toast.error(err.message, { position: "bottom-center" });
+        });
     }
 
     if(loading) return(
@@ -73,18 +100,19 @@ export const QnAPage = () => {
                     <Typography variant="h3" component="h3" color="primary" className={classes.title}>
                         Teams Sorting 
                     </Typography>
-                    <Carousel {...carouselSettings}> 
+                    <Carousel {...carouselSettings} ref={ref}> 
                         {questions.map((q, index) => {
                             return(
                                 <div key={q.qId}>
                                     <div className={classes.cover}>
                                     <FormLabel component="legend">{q.question}</FormLabel>
-                                        <RadioGroup aria-label="quiz" value={q.answers[0].ansId} onChange={handleChange} name="quiz">
+                                        <RadioGroup aria-label="quiz" value={values[index]} onChange={handleChange} name="quiz">
                                             <FormControlLabel value={q.answers[0].ansId} control={<Radio />} label={q.answers[0].answer}  />
                                             <FormControlLabel value={q.answers[1].ansId} control={<Radio />} label={q.answers[1].answer} />
                                             <FormControlLabel value={q.answers[2].ansId} control={<Radio />} label={q.answers[2].answer} />
                                             <FormControlLabel value={q.answers[3].ansId} control={<Radio />} label={q.answers[3].answer} />
                                     </RadioGroup>
+                                    {index===questions.length-1?<Button disabled={values.findIndex(v=>v===null)!==-1} onClick={submitHandler}>Submit</Button>:<Button onClick={()=>ref.current.slideNext()}>Next</Button>}
                                     </div>
                                 </div>
                             )
