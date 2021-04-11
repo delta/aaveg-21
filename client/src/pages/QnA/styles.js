e7bfd036c75029f93b3fdaf128d37b07e5ba3f76
Sneha <sneharmkrshnn@gmail.com>
2021-04-11 17:05:01 +0530

    [style] Questionnaire UI improvement

--- client/src/pages/QnA/styles.js
+++ client/src/pages/QnA/styles.js
@@ -1,34 +1,73 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-  title: {
-    margin: '10px'
-  },
-  main: {
-    position: 'fixed',
-    backgroundColor: 'black',
-    right: '0',
-    bottom: '0',
-    minWidth: '100%',
-    minHeight: '100%'
-  },
-  container: {
-    textAlign: 'center',
-    position: 'absolute',
-    top: '40%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    minWidth: '100em'
-  },
-  bgimg: {
-    position: 'fixed',
-    width: '100%',
-    height: '100%',
-    bottom: 0,
-    left: 0
-  },
-  cover: {
-    padding: '40px 30px',
-    fontSize: '18px'
-  }
-}))
+    title: {
+        margin: '10px',
+        color: 'rgba(255,255,255,0.8)',
+    },
+    main: {
+        position: 'fixed',
+        backgroundColor: 'black',
+        right: '0',
+        bottom: '0',
+        minWidth: '100%',
+        minHeight: '100%',
+    },
+    container: {
+        textAlign: 'center',
+        position: 'absolute',
+        top: '45%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        minWidth: '100em',
+    },
+    bgimg:{
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        bottom: 0,
+        left: 0,
+    },
+    cover: {
+        padding: '40px 30px',
+        fontSize: '18px',
+    },
+    legend: {
+        fontSize: '1.5rem',
+        marginBottom: '10px',
+        textAlign: 'end',
+        minWidth: '620px',
+        [theme.breakpoints.down(800)]: {
+            fontSize: '1.2rem',
+            // textAlign: 'center',
+            minWidth: 'calc(100vw - 200px)',
+        },
+    },
+    label: {
+        fontSize: '1.2rem',
+        [theme.breakpoints.down(800)]: {
+            fontSize: '1rem',
+        },
+    }, 
+    logo: {
+        marginBottom: '10px',
+    },
+    button: {
+        fontSize: '15px',
+        marginTop: '10px'
+    },
+    infoIcon: {
+        position: 'absolute',
+        top: '75px',
+        left: '15px',
+        cursor: 'pointer'
+    },
+
+    // For dialog box
+    dTitle: {
+        fontSize: '1.5rem'
+    },
+    dContent: {
+        fontSize: '1.2rem'
+    }
+}));
--- client/src/pages/QnA/utils/qList.js
+++ client/src/pages/QnA/utils/qList.js
@@ -1,131 +1,131 @@
 export const qList = [
-  {
-    qId: '1',
-    question: 'Question statement number 1 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '011',
-      answer: 'Answer 1 sfsrgkngksk'
+    {
+        qId: "1",
+        question: "You are stuck in the middle of the forest with no memory of how you got there. In front of you, you see three paths: left - towards a jungle, right - towards a beach and straight - towards a ruined castle. Which way do you go?",
+        answers: [{
+            ansId: "011",
+            answer: "Left" 
+        }, {
+            ansId: "012",
+            answer: "Straight" 
+        }, {
+            ansId: "013",
+            answer: "Right" 
+        }, {
+            ansId: "014",
+            answer: "Stay where you are" 
+        }]
     }, {
-      ansId: '012',
-      answer: 'Answer 2 sfsrgkngksk'
+        qId: "2",
+        question: "Question statement number 2 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "021",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "022",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "023",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "024",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '013',
-      answer: 'Answer 3 sfsrgkngksk'
+        qId: "3",
+        question: "Question statement number 3 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "031",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "032",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "033",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "034",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '014',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '2',
-    question: 'Question statement number 2 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '021',
-      answer: 'Answer 1 sfsrgkngksk'
+        qId: "4",
+        question: "Question statement number 4 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "041",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "042",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "043",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "044",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '022',
-      answer: 'Answer 2 sfsrgkngksk'
+        qId: "5",
+        question: "Question statement number 5 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "051",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "052",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "053",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "054",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '023',
-      answer: 'Answer 3 sfsrgkngksk'
+        qId: "6",
+        question: "Question statement number 6 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "061",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "062",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "063",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "064",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '024',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '3',
-    question: 'Question statement number 3 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '031',
-      answer: 'Answer 1 sfsrgkngksk'
+        qId: "7",
+        question: "Question statement number 7 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "071",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "072",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "073",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "074",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
     }, {
-      ansId: '032',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '033',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '034',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '4',
-    question: 'Question statement number 4 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '041',
-      answer: 'Answer 1 sfsrgkngksk'
-    }, {
-      ansId: '042',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '043',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '044',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '5',
-    question: 'Question statement number 5 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '051',
-      answer: 'Answer 1 sfsrgkngksk'
-    }, {
-      ansId: '052',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '053',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '054',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '6',
-    question: 'Question statement number 6 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '061',
-      answer: 'Answer 1 sfsrgkngksk'
-    }, {
-      ansId: '062',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '063',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '064',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '7',
-    question: 'Question statement number 7 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '071',
-      answer: 'Answer 1 sfsrgkngksk'
-    }, {
-      ansId: '072',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '073',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '074',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }, {
-    qId: '8',
-    question: 'Question statement number 8 njzd rufuwa eijeia fbr aud ekodedug beug?',
-    answers: [{
-      ansId: '081',
-      answer: 'Answer 1 sfsrgkngksk'
-    }, {
-      ansId: '082',
-      answer: 'Answer 2 sfsrgkngksk'
-    }, {
-      ansId: '083',
-      answer: 'Answer 3 sfsrgkngksk'
-    }, {
-      ansId: '084',
-      answer: 'Answer 4 sfsrgkngksk'
-    }]
-  }
+        qId: "8",
+        question: "Question statement number 8 njzd rufuwa eijeia fbr aud ekodedug beug?",
+        answers: [{
+            ansId: "081",
+            answer: "Answer 1 sfsrgkngksk" 
+        }, {
+            ansId: "082",
+            answer: "Answer 2 sfsrgkngksk" 
+        }, {
+            ansId: "083",
+            answer: "Answer 3 sfsrgkngksk" 
+        }, {
+            ansId: "084",
+            answer: "Answer 4 sfsrgkngksk" 
+        }]
+    }
 ]
